ab82f07a0eda833b0ecc94979630fd5d8d5aeee9
root <root@localhost>
2025-02-02 05:59:26 +0000

    CI changes

---
+++ config/routes.rb
@@ -0,0 +1,36 @@
+Rails.application.routes.draw do
+  root "main#index"
+
+  match 'about', to: "main#about", via: :get
+  match 'hello', to: "main#hello", via: :get
+
+  get 'main/index'
+
+  resources :categories do
+    member do
+      get :delete
+    end
+  end
+
+  resources :books do
+    member do
+      get :delete
+    end
+  end
+
+  # get 'books/index'
+  # get 'books/new'
+  # get 'books/edit'
+  # get 'books/show'
+  # get 'books/delete'
+  # get 'categories/index'
+  # get 'categories/show'
+  # get 'categories/new'
+  # get 'categories/edit'
+  # get 'categories/delete'
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
