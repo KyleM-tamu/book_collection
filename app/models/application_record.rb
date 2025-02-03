652d700f360c1b34d03057b7db50af813984d1dd
root <root@localhost>
2025-02-03 22:53:19 +0000

    Works on my machine

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
+  # has_many :books, through: :user_books
+  # has_many :user_books
+
   primary_abstract_class
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root "main#index"
+  resources :user_books
+  resources :users
+  root "user_books#index"
 
   match 'about', to: "main#about", via: :get
   match 'hello', to: "main#hello", via: :get
@@ -17,20 +19,5 @@ Rails.application.routes.draw do
       get :delete
     end
   end
-
-  # get 'books/index'
-  # get 'books/new'
-  # get 'books/edit'
-  # get 'books/show'
-  # get 'books/delete'
-  # get 'categories/index'
-  # get 'categories/show'
-  # get 'categories/new'
-  # get 'categories/edit'
-  # get 'categories/delete'
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # t
 end
